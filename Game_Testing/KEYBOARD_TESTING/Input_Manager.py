4f402abfe5b3e938ab96bfc4ab0e235dba13736a
Mallow-Mint <miran.lily1@gmail.com>
2024-10-20 17:02:17 +0800

    added damage calc for 'fire'

--- Game_Testing/KEYBOARD_TESTING/Input_Manager.py
+++ Game_Testing/KEYBOARD_TESTING/Input_Manager.py
@@ -1,4 +1,21 @@
 import pygame
+import random
+
+class Damage():
+    def __init__(self):
+        pass
+    def damage_range_calculator(self, base_damage):
+        self.damage_dealt = round(base_damage * random.uniform(0.8, 1.6), 0)
+        return self.damage_dealt
+
+    def critical_checker(self, damage_dealt):
+        critical_test = random.randint(1,16)
+        if critical_test == 16:
+            damage_dealt = damage_dealt*2
+        else:
+            pass
+
+        return damage_dealt
 
 class Spell:
     def __init__(self):
@@ -10,7 +27,11 @@ class Spell:
 
     def spellcast(self, spell):
         self.current_spell = spell
-        if spell in self.valid_words:
-            print("You Cast", self.current_spell)
+        match self.current_spell:
+            case 'fire':
+                self.fire_damage_dealt = damage.critical_checker(damage.damage_range_calculator(5))
+                print("You dealt", self.fire_damage_dealt, "damage")
+
 
 spell = Spell()
+damage = Damage()
