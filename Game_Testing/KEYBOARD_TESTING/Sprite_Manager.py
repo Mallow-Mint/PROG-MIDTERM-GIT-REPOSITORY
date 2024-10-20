e52e0716c213ce7fa9f2e75dd51b9847ff7ae0ee
Mallow-Mint <miran.lily1@gmail.com>
2024-10-20 15:37:09 +0800

    removed main file

--- Game_Testing/KEYBOARD_TESTING/Sprite_Manager.py
+++ Game_Testing/KEYBOARD_TESTING/Sprite_Manager.py
@@ -9,9 +9,8 @@ class Keyboard:
                               'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
                               'z', 'x', 'c', 'v', 'b', 'n', 'm' ]
         
-        
 class SpriteSheet():
-    def __init__(self, image):
+    def __init__(self, image):  
         self.sheet = image
 
     def get_keyboard_sprites(self):
