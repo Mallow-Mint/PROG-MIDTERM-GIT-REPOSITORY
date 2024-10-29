dded597c5b3955bfa55d41127d0e8ba745c56212
M4rmar <mpremedles.BSDSBA2028@aim.edu>
2024-10-29 12:10:20 +0800

    audio manager for the shop

---
+++ Game_Testing/SHOP TESTING/Audio_manager.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Music():
+    def __init__(self):
+        pass
+    def shop_bg_music(self):
+        shop_music = pygame.mixer.Sound('Game_Testing/SHOP TESTING/Assets/Stardew Valley OST.mp3')
+        shop_music.play(-1)
+    def buy_music(self):
+        buy_sound = pygame.mixer.Sound('Game_Testing/SHOP TESTING/Assets/buying sfx.mp3')
+        buy_sound.play()
+music = Music()
