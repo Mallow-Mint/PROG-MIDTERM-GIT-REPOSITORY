1fb918599b56e34d1d0c209b5ec5055952cda02c
Mallow-Mint <miran.lily1@gmail.com>
2024-11-03 12:51:28 +0800

    added sprites for potions

--- Game_Testing/SHOP TESTING/Spritesheet_manager.py
+++ Game_Testing/SHOP TESTING/Spritesheet_manager.py
@@ -4,43 +4,45 @@ import time
 PURPLE = (255, 0 , 255)
 run = True
 clock = pygame.time.Clock()
-animations_cooldown = 75
+animations_cooldown = 100
+potion_layer_test = pygame.Surface((1600, 900))
 
-def get_image(img:str, scale):
-    image = pygame.image.load(img).convert_alpha()
+
+def get_image(img, scale):
+    image = pygame.image.load(img)
     image = pygame.transform.scale_by(image, scale)
     return image
 
 class Spritesheet():
-    def __init__(self, image):
+    def __init__(self, image, width, height, frame_count, scale):
         self.sheet = image
-    
-    def get_potion_sprites_1(self):
-        self.animation_list_1 = []
-        self.animations_steps_1 = [15]
-        self.last_update_1 = pygame.time.get_ticks()
-        self.action_1 = 0
+        self.width = width
+        self.height = height
+        self.scale = scale
+        self.frames = frame_count
         self.frame_1 = 0
-        self.step_counter_1 = 0
-        self.potion_1_cordinates = [800, 450]
-    def Rollinganimation_1(self, WIDTH=1600, HEIGHT = 900):
-        self.potion_1_sprites = pygame.Surface((WIDTH, HEIGHT)).convert_alpha()
-        self.potion_1_sprites.fill(PURPLE)
-        for animation_1 in self.animations_steps_1:
-            temp_img_list_1 = []
-            for _ in range(animation_1):
-                img_1 = self.sheet# Adjust parameters as needed
-                if img_1 is not None:  # Ensure we have a valid image
-                    temp_img_list_1.append(img_1)
-                self.step_counter_1 += 1
-        self.animation_list_1.append(temp_img_list_1)
-        while run:
-            self.current_time_1 = pygame.time.get_ticks()
-            if self.current_time_1 - self.last_update_1 >= animations_cooldown:
-                self.frame_1 += 1
-                self.last_update_1 = self.current_time_1
-                if self.frame_1 >= len(self.animation_list_1[self.action_1]):
-                    self.frame_1 = 0
-        self.potion_1_sprites.blit(self.sheet, self.potion_1_cordinates)
-        
-        return self.potion_1_sprites
+        self.current_time_1 = 0
+        self.last_update_1 = pygame.time.get_ticks()
+
+    def get_potion_frames(self):
+        scaled_width = self.width * self.scale
+        scaled_height = self.height * self.scale
+        self.potion_1_frame_coordinates = []
+        for frame in range(self.frames):
+            current_frame = (scaled_width * frame, 0, scaled_width, scaled_height)
+            self.potion_1_frame_coordinates.append(current_frame)
+    
+    def get_single_frame(self):
+        potion_layer_test.blit(self.sheet, (0,0), self.potion_1_frame_coordinates[self.frame_1])
+
+    def get_potion_sprites_1(self):
+        if self.current_time_1 - self.last_update_1 >= animations_cooldown:
+            self.frame_1 += 1
+            self.last_update_1 = self.current_time_1
+            if self.frame_1 >= self.frames:
+                self.frame_1 = 0
+    
+    def display_potion(self):
+        self.current_time_1 = pygame.time.get_ticks()
+        self.get_potion_sprites_1()
+        self.get_single_frame()
